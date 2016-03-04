74291afe41187da53d050814834f7549b2539be1
Jürgen Heiling <juergen.heiling@gmail.com>
2016-03-04 11:34:55 +0100

    cleanup

--- src/SierpinskiCarpet/Main.elm
+++ src/SierpinskiCarpet/Main.elm
@@ -4,7 +4,7 @@ import SierpinskiCarpet.Model   exposing ( Model , defaultModel )
 import SierpinskiCarpet.View    exposing ( view )
 import SierpinskiCarpet.Interop exposing ( ModelJS , toModel )
 
-import Maybe                    exposing ( withDefault )
+import Maybe                    exposing ( map )
 import Signal                   exposing ( map2 , filterMap )
 import Graphics.Element         exposing ( Element )
 import Window                   exposing ( dimensions )
@@ -14,6 +14,6 @@ main : Signal Element
 main = map2 ( view ) dimensions model
 
 model : Signal Model
-model = filterMap ( Maybe.map toModel ) defaultModel loadModel
+model = filterMap ( map toModel ) defaultModel loadModel
 
 port loadModel : Signal ( Maybe ModelJS )
